186134b43b4e68001a18b0bb987242b9a2e968a1
TAT2003 <3347142262@qq.com>
2024-09-30 23:49:49 +0800

    finish errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,25 +7,34 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main()-> Result<(), Box<dyn std::error::Error>> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
+
+    //？是可能会提前终止程序返回Err的，所以main不能返回空
     let cost = total_cost(pretend_user_input)?;
 
+
+    
     if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
+            println!("You can't afford that many!");
+    }
+    else {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    Ok(())
+
+    
+
 }
 
-pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+pub fn total_cost(item_quantity: &str) -> Result<i32,ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>()?;
--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,9 +9,7 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-use std::num::ParseIntError;
+use std::{num::ParseIntError, string::ParseError};
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
@@ -26,13 +24,35 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+
+    fn from_parseint(err: ParseIntError)->ParsePosNonzeroError{
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    //let x: i64 = s.parse().unwrap();
+    //PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    
+    let r=s.parse::<i64>();
+
+    match r{
+        Ok(v)=>{
+            let r1=PositiveNonzeroInteger::new(v);
+            match r1{
+                Ok(v1)=>Ok(v1),
+                Err(e1)=>{
+                    Err(ParsePosNonzeroError::from_creation(e1))
+                }
+            }
+        }
+
+        Err(e)=>Err(ParsePosNonzeroError::from_parseint(e))
+    }
+
+
 }
 
 // Don't change anything below this line.
